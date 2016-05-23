82eaa43059325f6a5f36e8a1a6856d0cd67ed584
U-GDALTP\warmerdam <warmerdam@pobox.com>
2016-05-22 18:13:21 -0700

    preliminary attempt at python packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='GroundSurveyor',
+    version='0.1.0',
+    author='Brice Menard',
+    author_email='menard@jhu.edu',
+    packages=['ground_surveyor'],
+    scripts=['bin/compute_pile_statistics.py',
+             'bin/mosaic_tiles.py',
+             'bin/make_piles_of_unit_fields.py'],
+    #url='http://pypi.python.org/pypi/GroundSurveyor/',
+    url='http://github.com/bmenard1/GroundSurveyor/',
+    license='LICENSE.txt',
+    description='Deep imagery stack analysis and mosaicing.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        # Add gdal-python, opencv2, scipy, scipy.signal, scipy.ndimage, numpy
+    ],
+)
--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GS_TOP=$(cd $(dirname $0) && pwd)
-export PATH=$GS_TOP/scripts:$PATH
+export PATH=$GS_TOP/bin:$PATH
 export PYTHONPATH=${PYTHONPATH}${PYTHONPATH:+:}$GS_TOP
 
 bash -i
